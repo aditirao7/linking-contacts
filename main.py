f096b231e0688426f96ce35c5a12d763ca6012a5
aditirao7 <aditirao725@gmail.com>
2023-08-15 18:53:52 +0530

    Add logic for both new email and phone

--- main.py
+++ main.py
@@ -26,8 +26,13 @@ async def identify(info: Request):
     if 'phoneNumber' in keys:
         phoneNumber = req_info['phoneNumber']
 
+    emailList = []
+    phoneList = []
+    secondaryContactIds = []
+    primaryID = None
+
     # Algorithm for linking
-        # 6 types of valid input: Both New, Both Seen, New Email, New Phone, Seen Email with Null Phone, Seen Phone with Null Email
+    # 6 types of valid input: Both New, Both Seen, New Email, New Phone, Seen Email with Null Phone, Seen Phone with Null Email
     if email and phoneNumber:
         # For checking
         emailChain = db.query(Contact).filter(Contact.email == email)
@@ -38,7 +43,14 @@ async def identify(info: Request):
 
         # Both New: Insert as primary (CREATE)
         if emailChainSize == 0 and phoneChainSize == 0:
-            print(1)
+            newContact = Contact(**{'phoneNumber': phoneNumber, 'email': email, 'linkedId': None,
+                                    'linkPrecedence': 'primary', 'createdAt': datetime.now(), 'updatedAt': datetime.now()})
+            db.add(newContact)
+            db.commit()
+            db.refresh(newContact)
+            emailList.append(email)
+            phoneList.append(phoneNumber)
+            primaryID = newContact.id
 
         # New Email: Insert and Link to chain with same phone number (CREATE)
         if emailChainSize == 0 and phoneChainSize != 0:
@@ -73,9 +85,5 @@ async def identify(info: Request):
         # Seen Email/ Phone: Find all linked contacts and output (READ)
         print()
 
-    emailList = []
-    phoneList = []
-    secondaryContactIds = []
-
     # Return JSON response
-    return JSONResponse({"contact": {"primaryContactId": None, "emails": emailList, "phoneNumbers": phoneList, "secondaryContactIds": secondaryContactIds}})
+    return JSONResponse({"contact": {"primaryContactId": primaryID, "emails": emailList, "phoneNumbers": phoneList, "secondaryContactIds": secondaryContactIds}})
