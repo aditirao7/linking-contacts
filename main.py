d833ee5487db448808142ee3b3b310f2ff123385
aditirao7 <aditirao725@gmail.com>
2023-08-15 16:40:55 +0530

    Basic Identify API

--- main.py
+++ main.py
@@ -10,3 +10,13 @@ db = SessionLocal()
 @ app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/identify")
+async def identify(info: Request):
+    req_info = info.json()
+
+    email = req_info['email']
+    phoneNumber = req_info['phoneNumber']
+
+    return JSONResponse({"message": "Contacts API"})
