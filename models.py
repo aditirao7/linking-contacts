ab8bde1ea213d89b18dd49d5db73a3dd5d2a368c
aditirao7 <aditirao725@gmail.com>
2023-08-15 16:27:38 +0530

    Create Contact model

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import Column, DateTime, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -10,3 +10,19 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
+
+
+class Contact(Base):
+    __tablename__ = "contact"
+
+    id = Column(Integer, index=True, primary_key=True)
+    phoneNumber = Column(String)
+    email = Column(String)
+    linkedId = Column(Integer)
+    linkPrecedence = Column(String)
+    createdAt = Column(DateTime)
+    updatedAt = Column(DateTime)
+    deletedAt = Column(DateTime)
+
+
+Base.metadata.create_all(engine)
