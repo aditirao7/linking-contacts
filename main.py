eb70089745664a7a2bb1c37bbe0accf0d75777e3
aditirao7 <aditirao725@gmail.com>
2023-08-15 18:26:44 +0530

    Add conditions for null email or phone

--- main.py
+++ main.py
@@ -19,34 +19,41 @@ async def identify(info: Request):
     req_info = await info.json()
 
     # Get request info
-    email = req_info['email']
-    phoneNumber = req_info['phoneNumber']
+    email, phoneNumber = None, None
+    keys = req_info.keys()
+    if 'email' in keys:
+        email = req_info['email']
+    if 'phoneNumber' in keys:
+        phoneNumber = req_info['phoneNumber']
 
-    # Insert contact only if either phone or email is new
-    if len(list(db.query(Contact.id).filter(Contact.phoneNumber == phoneNumber))) == 0 or len(list(db.query(Contact.id).filter(Contact.email == email))) == 0:
+    # Algorithm for linking
+        # 6 types of valid input: Both New, Both Seen, New Email, New Phone, Seen Email with Null Phone, Seen Phone with Null Email
+    if email and phoneNumber:
+        # Checking if
+        # Both New: Insert as primary (CREATE)
+        # New Email: Insert and Link to chain with same phone number (CREATE)
+        # New Phone: Insert and Link to chain with same email (CREATE)
+        # Both Seen: Email and phone chains need to be linked, one chain is changed to all secondary by comparing ID (UPDATE)
+        # Insert contact only if either phone or email is new
         newContact = Contact(**{'phoneNumber': phoneNumber, 'email': email, 'linkedId': None,
                                 'linkPrecedence': 'primary', 'createdAt': datetime.now(), 'updatedAt': datetime.now()})
         db.add(newContact)
         db.commit()
         db.refresh(newContact)
 
-    # Checking if primary matches given input
-    primary = db.query(Contact).filter(or_(Contact.email == email, Contact.phoneNumber ==
-                                           phoneNumber), Contact.linkPrecedence == 'primary').first()
-    if primary is None:
-        newContact.linkedId = primary.id
-        newContact.linkPrecedence = 'secondary'
-        newContact.updatedAt = datetime.now()
-        db.add(newContact)
-        db.commit()
+        # Checking if primary matches given input
+        primary = db.query(Contact).filter(or_(Contact.email == email, Contact.phoneNumber ==
+                                               phoneNumber), Contact.linkPrecedence == 'primary').first()
+        if primary is None:
+            newContact.linkedId = primary.id
+            newContact.linkPrecedence = 'secondary'
+            newContact.updatedAt = datetime.now()
+            db.add(newContact)
+            db.commit()
 
-    # Algorithm for linking
-        # 6 types of valid input: Both New, Both Seen, New Email, New Phone, Seen Email with Null Phone, Seen Phone with Null Email
-        # Both New: Insert as primary (CREATE)
-        # New Email: Insert and Link to chain with same phone number (CREATE)
-        # New Phone: Insert and Link to chain with same email (CREATE)
-        # Both Seen: Email and phone chains need to be linked, one chain is changed to all secondary by comparing ID (UPDATE)
+    else:
         # Seen Email/ Phone: Find all linked contacts and output (READ)
+        print()
 
     emailList = []
     phoneList = []
